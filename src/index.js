f30c85eaaa6ad565954f074d6114cc84adb9070c
OgiogioVictor <ogiogiovictor@gmail.com>
2022-06-01 07:59:00 -0600

    working on UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
 
 function component() {
 
