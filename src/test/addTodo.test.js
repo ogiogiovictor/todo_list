2210e422a9d085eb302e1e692b99202d8526e96f
OgiogioVictor <ogiogiovictor@gmail.com>
2022-06-16 08:54:50 -0600

    pushing testing for add

--- src/test/addTodo.test.js
+++ src/test/addTodo.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import addTestTodo from '../modules/addtest_todo';
 import getTodos from '../modules/get_todo';
 import deleteTodo from '../modules/delete_todo';
@@ -9,10 +12,28 @@ describe('Add...', () => {
 
     test('Test Empty Localstorage', () => {
       document.body.innerHTML = '<ul class="todoList"></ul>';
-      const local = getTodos.allTodos();
-      //const local = localStorage.getItem('todos');
+      const local = localStorage.getItem('todos');
       expect(local).toBe(null);
     });
+
+    test('Test Empty DOM', () => {
+      document.body.innerHTML = '<ul class="todoList"></ul>';
+      getTodos.allTodos();
+      expect(document.body.querySelectorAll('li').length).toBe(0);
+    });
+
+    test('Test Adding Tasks', () => {
+      document.body.innerHTML = '<ul class="todoList"></ul>';
+       
+      const input = { value: 'Book 1' };
+      const input2 = { value: 'Book 2' };
+      addTestTodo(input);
+      addTestTodo(input2);
+      const local = JSON.parse(localStorage.getItem('todos'));
+      expect(local.length).toBe(2);
+    });
+  
+  
 
     
 });
